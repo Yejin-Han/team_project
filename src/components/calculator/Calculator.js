1cb08e2df81d3ecbaeb52243197f7adc915063df
Yejin-Han <yain3333@gmail.com>
2023-02-08 03:31:31 +0900

    230207 yejin update
    [수정]
    - Calculator.style.scss (.equals:hover 배경색)
    
    [추가]
    - CalcContext.js (calc, setCalc state 전역에서 사용하기 위해)
    - Keypads.js (버튼 클래스 이름 지정, 버튼 클릭 이벤트 추가 : C, . 클릭 구현)

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -1,29 +1,32 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./Calculator.style.scss";
 import Screen from "./Screen";
 import Keypads from "./Keypads";
-import "./Calculator.style.scss";
+import CalcProvider from "./CalcContext";
 
 const Calculator = () => {
   const navigate = useNavigate();
   return (
-    <div id="wrapper">
-      <div className="calc_top">
-        <div className="calc_header">
-          <h2>계산기</h2>
-          <button
-            type="button"
-            onClick={() => {
-              navigate("/");
-            }}
-          >
-            ✕
-          </button>
+    <CalcProvider>
+      <div id="wrapper">
+        <div className="calc_top">
+          <div className="calc_header">
+            <h2>계산기</h2>
+            <button
+              type="button"
+              onClick={() => {
+                navigate("/");
+              }}
+            >
+              ✕
+            </button>
+          </div>
+          <Screen />
         </div>
-        <Screen />
+        <Keypads />
       </div>
-      <Keypads />
-    </div>
+    </CalcProvider>
   );
 };
 
--- src/components/calculator/Keypads.js
+++ src/components/calculator/Keypads.js
@@ -1,3 +1,6 @@
+import { useContext } from "react";
+import { CalcContext } from "./CalcContext";
+
 const Values = [
   ["(", ")", "C", "del"],
   ["%", "𝓍²", "sqrt(x)", "÷"],
@@ -7,11 +10,77 @@ const Values = [
   ["+/-", "0", ".", "="],
 ];
 
-const Keypads = () => {
+const getStyleName = (btn) => {
+  const symbolClass = {
+    "(": "opt",
+    ")": "opt",
+    C: "opt",
+    del: "opt",
+    "%": "opt",
+    "𝓍²": "opt",
+    "sqrt(x)": "opt",
+    "÷": "opt",
+    x: "opt",
+    "-": "opt",
+    "+": "opt",
+    "=": "equals",
+  };
+  return symbolClass[btn];
+};
+
+const Keypads = ({ value }) => {
+  const { calc, setCalc } = useContext(CalcContext);
+  const numClick = (e) => {
+    const valueStr = value.toString();
+    let numValue;
+    if (valueStr === "0" && calc.num === 0) {
+      numValue = "0";
+    } else {
+      numValue = Number(calc.num + valueStr);
+    }
+  };
+  const btnClick = (e) => {};
+  const resetClick = () => {
+    setCalc({
+      sign: "",
+      num: 0,
+      res: 0,
+    });
+  };
+  const dotClick = () => {
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num, //screen에 .이 없으면 .을 붙여주고 아니면 그대로
+    });
+  };
+  const handleClick = () => {
+    const clickedValue = {
+      //"(": leftParenClick,
+      //")": rightParenClick,
+      C: resetClick,
+      //del: delClick,
+      //"%": percentClick,
+      //"𝓍²": powerClick,
+      //"sqrt(x)": rootClick,
+
+      ".": dotClick,
+    };
+    if (clickedValue[value]) {
+      return clickedValue[value]();
+    } else {
+      numClick();
+    }
+  };
+
   return (
     <div className="keypads">
       {Values.flat().map((btn, idx) => (
-        <button value={btn} key={idx} className="btn">
+        <button
+          value={btn}
+          key={idx}
+          className={`${getStyleName(btn)} btn`}
+          onClick={handleClick}
+        >
           {btn}
         </button>
       ))}
