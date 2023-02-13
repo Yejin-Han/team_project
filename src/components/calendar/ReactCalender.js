70b57b5b12080b3aef5c39c658440ecb7929b00f
Kang-Jiyeon <124341159+Kang-Jiyeon@users.noreply.github.com>
2023-02-13 23:26:26 +0900

    230213 jiyeon update
    [추가] 캘린더 하이라이트 기능, 지정된 날짜 배열 추가
    [수정] CSS파일 ReactCalender.js에 임포

--- src/components/calendar/ReactCalender.js
+++ src/components/calendar/ReactCalender.js
@@ -1,9 +1,29 @@
 import React, { useState } from "react";
 import Calendar from "react-calendar";
 import moment from "moment";
+import "./MainCalender.css";
 
 const ReactCalender = () => {
   const [date, setDate] = useState(new Date());
+  const holiDay = [
+    "01-01-2023",
+    "21-01-2023",
+    "22-01-2023",
+    "23-01-2023",
+    "24-01-2023",
+    "13-02-2023",
+    "01-03-2023",
+    "05-05-2023",
+    "27-05-2023",
+    "06-06-2023",
+    "15-08-2023",
+    "28-09-2023",
+    "29-09-2023",
+    "30-09-2023",
+    "03-10-2023",
+    "09-10-2023",
+    "25-12-2023",
+  ];
 
   const selectedDay = moment(date).format("YYYY-MM-DD");
 
@@ -11,7 +31,25 @@ const ReactCalender = () => {
     <div>
       <h1>React-calendar</h1>
       <div>
-        <Calendar onChange={setDate} value={date} />
+        <Calendar
+          onChange={setDate}
+          value={date}
+          locale="en-En"
+          tileClassName={({ date }) => {
+            let day = date.getDate(); //날짜 출력
+            let month = date.getMonth() + 1;
+            if (date.getMonth() < 10) {
+              month = "0" + month;
+            }
+            if (date.getDate() < 10) {
+              day = "0" + day;
+            }
+            const realDate = day + "-" + month + "-" + date.getFullYear();
+            if (holiDay.find((i) => i === realDate)) {
+              return "highlight";
+            }
+          }}
+        />
       </div>
       <div>
         <span>Selected Date : </span>
