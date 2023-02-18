0e12b03ff40f32c4ee0b5bd5ad75a1e2821c336e
Kang-Jiyeon <124341159+Kang-Jiyeon@users.noreply.github.com>
2023-02-18 21:32:32 +0900

    230218 jiyeon update
    [추가] 환율 계산식
    [추가] select box에서 수취국가, 송금국가 선택해 환율 표시

--- src/components/converter/Converter.js
+++ src/components/converter/Converter.js
@@ -5,6 +5,21 @@ const BASE_URL = "https://api.exchangerate.host/latest";
 
 function Converter() {
   const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
+  const [exchangeRate, setExchangeRate] = useState();
+  const [amount, setAmount] = useState(1);
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true);
+
+  let toAmount, fromAmount;
+  if (amountInFromCurrency) {
+    fromAmount = amount; // 시작금액
+    toAmount = amount * exchangeRate; // 시작금액 * 환율금액
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exchangeRate;
+  }
+
   useEffect(() => {
     fetch(BASE_URL)
       .then((res) => res.json())
@@ -17,12 +32,43 @@ function Converter() {
       });
   }, []);
 
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+      // console.log(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`);
+      fetch(`${BASE_URL}?base=${fromCurrency}&symbols=${toCurrency}`)
+        .then((res) => res.json())
+        .then((data) => setExchangeRate(data.rates[toCurrency]));
+    }
+  }, [fromCurrency, toCurrency]);
+
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(true);
+  }
+
+  function handleToAmountChange(e) {
+    setAmount(e.target.value);
+    setAmountInFromCurrency(false);
+  }
+
   return (
     <>
       <h1>환율계산기</h1>
-      <Currency currencyOptions={currencyOptions} />
+      <Currency
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+        onChangeCurrency={(e) => setFromCurrency(e.target.value)}
+        onChangeAmount={handleFromAmountChange}
+        amount={fromAmount}
+      />
       <div className="con_equals">=</div>
-      <Currency currencyOptions={currencyOptions} />
+      <Currency
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={(e) => setToCurrency(e.target.value)}
+        onChangeAmount={handleToAmountChange}
+        amount={toAmount}
+      />
     </>
   );
 }
--- src/components/converter/Currency.js
+++ src/components/converter/Currency.js
@@ -1,12 +1,23 @@
 import React from "react";
 
 export default function Currency(props) {
-  const { currencyOptions } = props;
+  const {
+    currencyOptions,
+    selectedCurrency,
+    onChangeCurrency,
+    onChangeAmount,
+    amount,
+  } = props;
 
   return (
     <div>
-      <input type="number" className="input" />
-      <select>
+      <input
+        type="number"
+        className="input"
+        value={amount}
+        onChange={onChangeAmount}
+      />
+      <select value={selectedCurrency} onChange={onChangeCurrency}>
         {React.Children.toArray(
           currencyOptions.map((option, key) => (
             <option key={key} value={option}>
