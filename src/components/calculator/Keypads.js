d2cc7bfd80f3b477a9d60306c8c20516b7a03bd4
Yejin-Han <yain3333@gmail.com>
2023-02-18 15:44:55 +0900

    230218 yejin update1
    [수정]
    - Keypads.js 전면 수정 signClick 빼고 완료

--- src/components/calculator/Keypads.js
+++ src/components/calculator/Keypads.js
@@ -21,92 +21,6 @@ const getStyleName = (btn) => {
 
 const Keypads = ({ value }) => {
   const { calc, setCalc } = useContext(CalcContext);
-  const numClick = () => {
-    const valueStr = value.toString();
-    let numValue;
-    if (valueStr === "0" && calc.num === 0) {
-      numValue = "0";
-    } else {
-      numValue = Number(calc.num + valueStr);
-    }
-    setCalc({
-      ...calc,
-      num: numValue,
-      exp: calc.exp ? valueStr : calc.exp + valueStr,
-      /* calc.exp.toString().includes("=") ? valueStr : calc.exp !== 0 ? calc.exp + valueStr : valueStr, */
-    });
-  };
-  const percentClick = () => {
-    setCalc({
-      num: calc.num / 100,
-      res: calc.res / 100,
-      sign: "",
-      exp: calc.exp ? calc.num + "÷100" : 0,
-    });
-  };
-  const piClick = () => {
-    setCalc({
-      num: calc.num ? (calc.num * Math.PI).toFixed(10) : Math.PI.toFixed(10),
-      res: calc.res ? (calc.res * Math.PI).toFixed(10) : Math.PI.toFixed(10),
-      sign: "",
-      exp: calc.num ? calc.num + value : value,
-    });
-  };
-  const resetClick = () => {
-    setCalc({
-      sign: "",
-      num: 0,
-      res: 0,
-      exp: 0,
-    });
-  };
-  const delClick = () => {
-    setCalc({
-      num: calc.num ? calc.num.toString().slice(0, -1) : 0,
-      res: calc.res ? calc.res.toString().slice(0, -1) : 0,
-      sign: "",
-      exp: calc.exp ? calc.exp.toString().slice(0, -1) : 0,
-    });
-  };
-  const squareClick = () => {
-    setCalc({
-      num: calc.num ** 2,
-      res: calc.res ** 2,
-      sign: "",
-      exp: calc.num ? calc.num + "^2" : calc.res + "^2",
-    });
-  };
-  const sqrtClick = (e) => {
-    setCalc({
-      num: Number(Math.sqrt(calc.num).toFixed(10)),
-      res: Number(Math.sqrt(calc.res).toFixed(10)),
-      sign: "",
-      exp: calc.num ? "√" + calc.num : "√" + calc.res,
-    });
-  };
-  const signClick = () => {
-    setCalc({
-      sign: value,
-      res: !calc.res && calc.num ? calc.num : calc.res,
-      num: 0,
-      exp: !calc.res && calc.num ? calc.num + value : calc.res + value,
-    });
-  };
-  const invertClick = () => {
-    setCalc({
-      num: calc.num ? calc.num * -1 : 0,
-      res: calc.res ? calc.res * -1 : 0,
-      sign: "",
-      exp: calc.num ? calc.num + "* (-1)" : calc.res + "* (-1)",
-    });
-  };
-  const dotClick = () => {
-    setCalc({
-      ...calc,
-      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num, //screen에 .이 없으면 .을 붙여주고 아니면 그대로
-      exp: calc.exp ? calc.exp + value : value,
-    });
-  };
   const equalsClick = () => {
     if (calc.res && calc.num) {
       const math = (a, b, sign) => {
@@ -126,12 +40,153 @@ const Keypads = ({ value }) => {
       });
     }
   };
+  const numClick = () => {
+    const valueStr = value.toString();
+    let numValue;
+    if (valueStr === "0" && calc.num === 0) {
+      numValue = "0";
+    } else {
+      numValue = Number(calc.num + valueStr);
+    }
+    setCalc({
+      ...calc,
+      num:
+        !calc.exp || calc.exp.toString().includes("=")
+          ? Number(valueStr)
+          : numValue,
+      res: !calc.exp || calc.exp.toString().includes("=") ? 0 : calc.res,
+      exp:
+        !calc.exp || calc.exp.toString().includes("=")
+          ? valueStr
+          : calc.exp + valueStr,
+    });
+  };
+  const percentClick = () => {
+    setCalc({
+      num: calc.num / 100,
+      res: calc.res / 100,
+      sign: "",
+      exp: calc.exp ? calc.num + "÷100=" : 0,
+    });
+  };
+  const piClick = () => {
+    setCalc({
+      num: calc.num ? (calc.num * Math.PI).toFixed(10) : Math.PI.toFixed(10),
+      res: calc.res ? (calc.res * Math.PI).toFixed(10) : Math.PI.toFixed(10),
+      sign: "",
+      exp: calc.num ? calc.num + value : value,
+    });
+  };
+  const resetClick = () => {
+    setCalc({
+      sign: "",
+      num: 0,
+      res: 0,
+      exp: 0,
+    });
+  };
+  const delClick = () => {
+    const expStr = calc.exp.toString();
+    if (expStr.includes("=") || !calc.exp) {
+      setCalc({
+        num: 0,
+        res: 0,
+        sign: "",
+        exp: 0,
+      });
+    } else if (
+      expStr.substr(expStr.length - 1, 1) === "÷" ||
+      expStr.substr(expStr.length - 1, 1) === "x" ||
+      expStr.substr(expStr.length - 1, 1) === "-" ||
+      expStr.substr(expStr.length - 1, 1) === "+"
+    ) {
+      setCalc({
+        num: calc.num,
+        res: calc.res,
+        sign: "",
+        exp: calc.exp.toString().slice(0, -1),
+      });
+    } else {
+      setCalc({
+        num: calc.num.toString().slice(0, -1),
+        res: calc.res.toString().slice(0, -1),
+        sign: "",
+        exp: calc.exp.toString().slice(0, -1),
+      });
+    }
+  };
+  const fractionClick = () => {
+    setCalc({
+      num: (1 / calc.num).toFixed(10),
+      res: (1 / calc.res).toFixed(10),
+      sign: "",
+      exp: calc.num ? "1÷" + calc.num + "=" : "1÷" + calc.res + "=",
+    });
+  };
+  const squareClick = () => {
+    setCalc({
+      num: calc.num ** 2,
+      res: calc.res ** 2,
+      sign: "",
+      exp: calc.num ? calc.num + "^2=" : calc.res + "^2=",
+    });
+  };
+  const sqrtClick = () => {
+    setCalc({
+      num: Number(Math.sqrt(calc.num).toFixed(10)),
+      res: Number(Math.sqrt(calc.res).toFixed(10)),
+      sign: "",
+      exp: calc.num ? "√" + calc.num + "=" : "√" + calc.res + "=",
+    });
+  };
+  const signClick = () => {
+    //너 미완이야 일단 치울게
+    console.log(
+      calc.exp.toString().includes("÷") ||
+        calc.exp.toString().includes("x") ||
+        calc.exp.toString().includes("-") ||
+        calc.exp.toString().includes("+"),
+      calc.num,
+      calc.res,
+      calc.exp
+    );
+    if (
+      calc.exp.toString().includes("÷") ||
+      calc.exp.toString().includes("x") ||
+      calc.exp.toString().includes("-") ||
+      calc.exp.toString().includes("+")
+    ) {
+      equalsClick();
+    }
+    setCalc({
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0,
+      exp: !calc.res && calc.num ? calc.num + value : calc.res + value,
+    });
+  };
+  const invertClick = () => {
+    setCalc({
+      num: calc.num ? calc.num * -1 : 0,
+      res: calc.res ? calc.res * -1 : 0,
+      sign: "",
+      exp: calc.num ? calc.num + "*(-1)=" : calc.res + "*(-1)=",
+    });
+  };
+  const dotClick = () => {
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num, //screen에 .이 없으면 .을 붙여주고 아니면 그대로
+      exp: calc.exp ? calc.exp + value : value,
+    });
+  };
   const handleClick = () => {
     const clickedValue = {
       "%": percentClick,
       π: piClick,
       C: resetClick,
       del: delClick,
+      "1/𝓍": fractionClick,
       "𝓍²": squareClick,
       "√𝓍": sqrtClick,
       "÷": signClick,
