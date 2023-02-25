d055c6087c558ae20a74eb19bd72f42e69a21497
Yejin-Han <yain3333@gmail.com>
2023-02-25 18:38:19 +0900

    230225 yejin update
    [수정]
    - Weather.js (GetAPI 연결, 제목과 X버튼)
    
    [추가]
    - GetAPI.js (openweathermap api와 axios를 이용하여 날씨 정보를 불러옴, 도시 선택, 기본 날씨 정보 출력)

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,7 +1,27 @@
+import { useNavigate } from "react-router-dom";
 import "./Weather.style.scss";
+import GetAPI from "./GetAPI";
 
 const Weather = () => {
-  return <div>dsf</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className="weather_top">
+        <h2>세계날씨 🌤️</h2>
+        <button
+          type="button"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          ✕
+        </button>
+      </div>
+      <div className="weather_content">
+        <GetAPI />
+      </div>
+    </>
+  );
 };
 
 export default Weather;
