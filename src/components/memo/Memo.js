62fbc50f44a3be888deb6125f4c9893ac2cb8e52
Kang-Jiyeon <124341159+Kang-Jiyeon@users.noreply.github.com>
2023-03-01 18:40:06 +0900

    Revert "230301 jiyeon update"
    This reverts commit 1d37594964d151a745b8b7af9868a6319da2ae1a.

--- src/components/memo/Memo.js
+++ src/components/memo/Memo.js
@@ -1,19 +1,45 @@
-import { useRef } from "react";
-import Draggable from "./component/draggable";
+import React, { useRef, useState } from "react";
+import MemoEditor from "./MemoEditor";
+import MemoList from "./MemoList";
+import "./memo.style.scss";
+const Memo = () => {
+  const [data, setData] = useState([]);
+  const dataId = useRef(0);
 
-function Memo() {
-  const handle = useRef(null);
+  const createData = (title, content) => {
+    //새로운 데이터 추가하기
+    const date = new Date().getTime(); //현재 시간 가져오기
+    const newItem = {
+      title,
+      content,
+      date,
+      id: dataId.current, //0
+    };
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  };
+
+  localStorage.setItem("list", JSON.stringify(data));
+
+  const dataDelete = (targetId) => {
+    const newMemoList = data.filter((i) => i.id !== targetId);
+    setData(newMemoList);
+  };
+
+  const dataEdit = (targetId, newContent) => {
+    //수정대상 아이디, 수정될 데이터를 인자로 받아오기
+    setData(
+      //수정대상 아이디와 일치하는 대상 찾기
+      data.map((i) => (i.id === targetId ? { ...i, content: newContent } : i))
+    );
+  };
+
   return (
-    <>
-      <Draggable handleRef={handle}>
-        <div style={{ width: "100px", height: "100px", background: "#cd7d9f" }}>
-          <button ref={handle} style={{ color: "#fff" }}>
-            Move
-          </button>
-        </div>
-      </Draggable>
-    </>
+    <div className="main_memo">
+      <MemoEditor createData={createData} />
+      <MemoList dataEdit={dataEdit} dataDelete={dataDelete} memoList={data} />
+    </div>
   );
-}
+};
 
 export default Memo;
