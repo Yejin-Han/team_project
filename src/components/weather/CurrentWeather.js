4d8e616a0a07766d1b66cabafb2254f55041cf0d
Yejin-Han <yain3333@gmail.com>
2023-03-07 04:38:22 +0900

    230307 yejin update (날씨 완)
    [수정]
    - Weather.style.scss (.curr_weather 구조 재배치, accordion grid)
    
    [추가]
    - Forecast.js (accordion 클릭하면 세부정보 보이기 - 풍향, 풍속, 습도, 기압, 풍향은 deg체크해서 한글로 변환, 현재 날짜와 시간 추가)
    - CurrentWeather.js (최고기온, 최저기온 추가, useState로 curr_weather 정보 새로고침 가능, 이모지로 변경)

--- src/components/weather/CurrentWeather.js
+++ src/components/weather/CurrentWeather.js
@@ -1,10 +1,49 @@
+import { useState } from "react";
+
+let today = "";
+const dayCheck = (todayNum) => {
+  switch (todayNum) {
+    case 0:
+      today = "일";
+      break;
+    case 1:
+      today = "월";
+      break;
+    case 2:
+      today = "화";
+      break;
+    case 3:
+      today = "수";
+      break;
+    case 4:
+      today = "목";
+      break;
+    case 5:
+      today = "금";
+      break;
+    default:
+      today = "토";
+  }
+};
+
 const CurrentWeather = ({ data }) => {
+  const [date, setDate] = useState(new Date());
+  const [temp, setTemp] = useState(Math.round(data.main.temp));
+  const [maxTemp, setMaxTemp] = useState(Math.round(data.main.temp_max));
+  const [minTemp, setMinTemp] = useState(Math.round(data.main.temp_min));
+  const [feelsLike, setFeelsLike] = useState(Math.round(data.main.feels_like));
+
+  const todayNum = date.getDay();
+  dayCheck(todayNum);
+  const nowHour = date.getHours();
+  const nowMin = date.getMinutes();
+
   return (
     <div className="curr_weather">
       <div className="curr_top">
         <div>
+          <p className="temperature">{temp} °</p>
           <p className="city">{data.city}</p>
-          <p className="weather_description">{data.weather[0].description}</p>
         </div>
         <img
           src={`img/weather/${data.weather[0].icon}.png`}
@@ -13,28 +52,51 @@ const CurrentWeather = ({ data }) => {
         />
       </div>
       <div className="curr_bottom">
-        <p className="temperature">{Math.round(data.main.temp)} °</p>
         <div className="details">
           <div className="param_row">
-            <span className="param_label">세부날씨</span>
+            <div>
+              <span className="param_value">
+                {maxTemp}° / {minTemp}°
+              </span>
+            </div>
+            <div>
+              <span className="param_label">체감온도</span>{" "}
+              <span className="param_value">{feelsLike}°</span>
+            </div>
           </div>
           <div className="param_row">
-            <span className="param_label">체감온도</span>
-            <span className="param_value">
-              {Math.round(data.main.feels_like)}°
+            <span>{today},&nbsp;</span>
+            <span>
+              {nowHour < 12
+                ? `오전 ${nowHour}:${nowMin} 기준`
+                : `오후 ${nowHour}:${nowMin} 기준`}
+            </span>
+            <span
+              className="resetWeatherInfo"
+              onClick={() => {
+                setDate(new Date());
+                setTemp(Math.round(data.main.temp));
+                setMaxTemp(Math.round(data.main.temp_max));
+                setMinTemp(Math.round(data.main.temp_min));
+                setFeelsLike(Math.round(data.main.feels_like));
+              }}
+            >
+              &nbsp;🔃
             </span>
           </div>
           <div className="param_row">
-            <span className="param_label">습도</span>
-            <span className="param_value">{data.main.humidity}%</span>
-          </div>
-          <div className="param_row">
-            <span className="param_label">바람</span>
-            <span className="param_value">{data.wind.speed}m/s</span>
-          </div>
-          <div className="param_row">
-            <span className="param_label">기압</span>
-            <span className="param_value">{data.main.pressure}hPa</span>
+            <div>
+              <span className="param_label">💧&nbsp;</span>
+              <span className="param_value">{data.main.humidity}%</span>
+            </div>
+            <div>
+              <span className="param_label">🌬️&nbsp;</span>
+              <span className="param_value">{data.wind.speed}m/s</span>
+            </div>
+            <div>
+              <span className="param_label">⬇️&nbsp;</span>
+              <span className="param_value">{data.main.pressure}hPa</span>
+            </div>
           </div>
         </div>
       </div>
--- src/components/weather/Forecast.js
+++ src/components/weather/Forecast.js
@@ -17,11 +17,71 @@ const timeLine = [
   "오후 9시",
 ];
 
+let idx = 0;
+const timeFor3Hours = (now) => {
+  if (now >= 0 && now < 3) {
+    idx = 0;
+  } else if (now >= 3 && now < 6) {
+    idx = 1;
+  } else if (now >= 6 && now < 9) {
+    idx = 2;
+  } else if (now >= 9 && now < 12) {
+    idx = 3;
+  } else if (now >= 12 && now < 15) {
+    idx = 4;
+  } else if (now >= 18 && now < 21) {
+    idx = 5;
+  } else if (now >= 21 && now < 24) {
+    idx = 6;
+  }
+};
+
+const degCheck = (deg) => {
+  if (deg >= 348.75 || deg < 11.25) {
+    return <label>북</label>;
+  } else if (deg >= 11.25 && deg < 33.75) {
+    return <label>북북동</label>;
+  } else if (deg >= 33.75 && deg < 56.25) {
+    return <label>북동</label>;
+  } else if (deg >= 56.25 && deg < 78.75) {
+    return <label>동북동</label>;
+  } else if (deg >= 78.75 && deg < 101.25) {
+    return <label>동</label>;
+  } else if (deg >= 101.25 && deg < 123.75) {
+    return <label>동남동</label>;
+  } else if (deg >= 123.75 && deg < 146.25) {
+    return <label>남동</label>;
+  } else if (deg >= 146.25 && deg < 168.75) {
+    return <label>남남동</label>;
+  } else if (deg >= 168.75 && deg < 191.25) {
+    return <label>남</label>;
+  } else if (deg >= 191.25 && deg < 213.75) {
+    return <label>남남서</label>;
+  } else if (deg >= 213.75 && deg < 236.25) {
+    return <label>남서</label>;
+  } else if (deg >= 236.25 && deg < 258.75) {
+    return <label>서남서</label>;
+  } else if (deg >= 258.75 && deg < 281.25) {
+    return <label>서</label>;
+  } else if (deg >= 281.25 && deg < 303.75) {
+    return <label>서북서</label>;
+  } else if (deg >= 303.75 && deg < 326.25) {
+    return <label>북서</label>;
+  } else if (deg >= 326.25 && deg < 348.75) {
+    return <label>북북서</label>;
+  }
+};
+
 const Forecast = ({ data }) => {
+  const timeOfNow = new Date().getHours();
+  timeFor3Hours(timeOfNow);
+  const forecastTimeLine = timeLine
+    .slice(idx - 1, timeLine.length)
+    .concat(timeLine.slice(0, idx - 1));
   return (
     <>
-      {/* 오늘 0시~21시까지의 예상날씨 3시간단위로 */}
-      <label className="forecast_title">오늘 예상날씨</label>
+      {/* 직전 3시간+24시간까지의 예상날씨 3시간단위로 */}
+      <label className="forecast_title">시간별 예상날씨</label>
       <Accordion allowZeroExpanded className="forecast_accordion">
         {data.list.slice(1, 8).map((item, idx) => (
           <AccordionItem key={idx}>
@@ -33,17 +93,33 @@ const Forecast = ({ data }) => {
                     className="icon_s"
                     src={`img/weather/${item.weather[0].icon}.png`}
                   />
-                  <label className="times">{timeLine[idx]}</label>
-                  <label className="description">
-                    {item.weather[0].description}
-                  </label>
-                  <label className="avg_temp">
-                    {Math.round((item.main.temp_max + item.main.temp_min) / 2)}°
+                  <label className="times">{forecastTimeLine[idx]}</label>
+                  <label className="time_temp">
+                    {Math.round(item.main.temp)}°
                   </label>
                 </div>
               </AccordionItemButton>
             </AccordionItemHeading>
-            <AccordionItemPanel></AccordionItemPanel>
+            <AccordionItemPanel>
+              <div className="time_details_grid">
+                <div className="time_details_grid_item">
+                  <label>풍향</label>
+                  {degCheck(item.wind.deg)}
+                </div>
+                <div className="time_details_grid_item">
+                  <label>풍속</label>
+                  {item.wind.speed}m/s
+                </div>
+                <div className="time_details_grid_item">
+                  <label>습도</label>
+                  {item.main.humidity}%
+                </div>
+                <div className="time_details_grid_item">
+                  <label>기압</label>
+                  {item.main.pressure}hPa
+                </div>
+              </div>
+            </AccordionItemPanel>
           </AccordionItem>
         ))}
       </Accordion>
